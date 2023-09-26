15c18802e4f576f34d1ad374cedcccfa8cd92c7e
Bálint Hejner <hejnerbalint@gmail.com>
2023-09-26 11:20:26 +0200

    Example task for Validation

---
+++ Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_WPF_Konyvek
+{
+    public class Book
+    {
+		private string _title;
+
+		public string Title
+		{
+			get { return _title; }
+			set { _title = value; }
+		}
+
+		private int _pagecount;
+
+		public int Pagecount
+		{
+			get { return _pagecount; }
+			set { _pagecount = value; }
+		}
+
+		private string _author;
+
+		public string Author
+		{
+			get { return _author; }
+			set { _author = value; }
+		}
+
+		private string _type;
+
+		public string Type
+		{
+			get { return _type; }
+			set { _type = value; }
+		}
+
+
+        public Book(string row)
+        {
+			string[] datas = row.Split(';');
+			Title = datas[0];
+			Pagecount = int.Parse(datas[1]);
+			Author = datas[2];
+			Type = datas[3];
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace _07_WPF_Konyvek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Book> books = new List<Book>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            loadBooks();
+        }
+
+        private void loadBooks()
+        {
+            foreach (var row in File.ReadAllLines("booklist.csv").Skip(1))
+            {
+                books.Add(new Book(row));
+            } 
+        }
+    }
+}
